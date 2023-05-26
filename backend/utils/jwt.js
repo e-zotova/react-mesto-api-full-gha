822524f4e5b23b891fcbeebcfdbf2bea9f8481d9
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-05-26 16:45:04 +0400

    log node_env

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -2,7 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-const getJwtToken = (id) => jwt.sign({ id }, NODE_ENV === 'production' ? JWT_SECRET : 'someveryveryverysecretkey', { expiresIn: '7d' });
+const getJwtToken = (id) => jwt.sign(
+  { id },
+  NODE_ENV === 'production' ? JWT_SECRET : 'someveryveryverysecretkey',
+  { expiresIn: '7d' },
+);
 const verifyJwtToken = (token) => jwt.verify(token, JWT_SECRET);
+
+console.log(process.env.NODE_ENV);
 
 module.exports = { getJwtToken, verifyJwtToken };
