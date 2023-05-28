449e108562198361c266d780029e3f3e7c18e5b7
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-05-28 11:16:40 +0400

    fix: crash test

--- backend/app.js
+++ backend/app.js
@@ -23,14 +23,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
+
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-
 app.use(helmet());
 app.use(limiter);
+
 app.use(router);
 app.use(errorLogger);
 app.use(handleErrorMiddleware);
